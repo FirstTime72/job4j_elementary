fe599df3701b1decfeb23878fe7d2a8ea14c9730
FirstTime72 <artem.matyushov@mail.ru>
2022-07-16 23:57:18 +0500

    1.2. Входные данные в тесте [#297750]

---
+++ src/test/java/ru/job4j/condition/X2Test.java
@@ -0,0 +1,62 @@
+package ru.job4j.condition;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class X2Test {
+
+    @Test
+    public void whenA10B0C0X2Then40() {
+        int a = 10;
+        int b = 0;
+        int c = 0;
+        int x = 2;
+        int expected = 40;
+        int rsl = X2.calc(a, b, c, x);
+        Assert.assertEquals(expected, rsl);
+    }
+
+    @Test
+    public void whenA1B1C1X1Then3() {
+        int a = 1;
+        int b = 1;
+        int c = 1;
+        int x = 1;
+        int expected1 = 3;
+        int rsl1 = X2.calc(a, b, c, x);
+        Assert.assertEquals(expected1, rsl1);
+    }
+
+    @Test
+    public void whenA0B1C1X1Then2() {
+        int a = 0;
+        int b = 1;
+        int c = 1;
+        int x = 1;
+        int expected2 = 2;
+        int rsl2 = X2.calc(a, b, c, x);
+        Assert.assertEquals(expected2, rsl2);
+    }
+
+    @Test
+    public void whenA1B1C0X1Then2() {
+        int a = 1;
+        int b = 1;
+        int c = 0;
+        int x = 1;
+        int expected3 = 2;
+        int rsl3 = X2.calc(a, b, c, x);
+        Assert.assertEquals(expected3, rsl3);
+    }
+
+    @Test
+    public void whenA1B1C1X0Then2() {
+        int a = 1;
+        int b = 1;
+        int c = 1;
+        int x = 0;
+        int expected4 = 1;
+        int rsl4 = X2.calc(a, b, c, x);
+        Assert.assertEquals(expected4, rsl4);
+    }
+}
