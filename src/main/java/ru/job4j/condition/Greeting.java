51c77fc31fa2658fb7e24c81a38fd68da92680d6
FirstTime72 <artem.matyushov@mail.ru>
2022-07-16 11:27:27 +0500

    9.1. String [#237407]

---
+++ src/main/java/ru/job4j/condition/Greeting.java
@@ -0,0 +1,12 @@
+package ru.job4j.condition;
+
+public class Greeting {
+    public static void main(String[] args) {
+        String idea = "I like Java! ";
+        System.out.println(idea);
+        idea += "But i am newbie ";
+        int yaer = 2022;
+        String total = idea + yaer;
+        System.out.println(total);
+    }
+}
