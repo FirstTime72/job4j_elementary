b80c24b641c7db22fe204c335ac2f44b566db7fa
FirstTime72 <artem.matyushov@mail.ru>
2022-07-17 20:51:40 +0500

    0. Операции сравнения [#235631]

---
+++ src/main/java/ru/job4j/condition/Liken.java
@@ -0,0 +1,17 @@
+package ru.job4j.condition;
+
+public class Liken {
+    static public void main(String[] args) {
+        int first = 10;
+        int second = 9;
+        boolean result = first > second;
+        System.out.println(result);
+
+        boolean result1 = first < second;
+        System.out.println(result1);
+
+        boolean result2 = first == second;
+        System.out.println(result2);
+
+    }
+}
