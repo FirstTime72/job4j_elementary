08595b4e9dd1bb3476aa6b8229daf2958d5f5418
FirstTime72 <artem.matyushov@mail.ru>
2022-07-21 11:52:18 +0500

    ABC

--- src/main/java/ru/job4j/condition/ABC.java
+++ src/main/java/ru/job4j/condition/ABC.java
@@ -2,8 +2,8 @@ package ru.job4j.condition;
 
 public class ABC {
 
-    public static int findBiggestOfThree(int A, int B, int C) {
-        return A > B ? A > C ? A : C : B > C ? B : C;
+    public static int findBiggestOfThree(int a, int b, int c) {
+        return a > b ? a > c ? a : c : b > c ? b : c;
     }
 
     public static void main(String[] args) {
