8c04b7c1ed4c0dc9ce3840d27324a525614a8ae3
FirstTime72 <artem.matyushov@mail.ru>
2022-08-12 13:54:14 +0500

    Fitness - Циклы

---
+++ src/main/java/ru/job4j/loop/Fitness.java
@@ -0,0 +1,21 @@
+package ru.job4j.loop;
+
+public class Fitness {
+    public static int calc(int ivan, int nik) {
+        int month = 0;
+        while (nik >= ivan) {
+            month ++;
+            ivan *= 3;
+            nik *= 2;
+        }
+        return month;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(calc(95, 90));
+        System.out.println(calc(90, 95));
+        System.out.println(calc(50, 90));
+        System.out.println(calc(90, 90));
+        System.out.println(calc(30, 300));
+    }
+}
